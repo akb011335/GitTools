12f46d843037262c6548b1fbd734b28ffe4414f2
Yuiki <akb011335@gmail.com>
2024-07-05 09:41:42 +0800

    step1

--- github_set_global_user_detail.bat
+++ github_set_global_user_detail.bat
@@ -13,9 +13,10 @@
 chcp 65001
 
 :: Git 使用者名稱
-set user_name=Ken Chang
+set user_name=Yuiki
+
 :: Git 使用者信箱
-set user_email=keoinn@gmail.com
+set user_email=akb011335@gmail.com
 
 git config --global user.name "%user_name%"
 git config --global user.email "%user_email%"
